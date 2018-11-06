d26724cea94798982e24746b80d69ae149bd55ca
mkh <mohamadekhalili2020@gmail.com>
2018-11-06 20:00:49 +0330

    p1

--- downloader_class.cpp
+++
@@ -1,6 +0,0 @@
-#include "downloader_class.h"
-
-Downloader_Class::Downloader_Class(QObject *parent) : QObject(parent)
-{
-
-}
--- testclass.cpp
+++
@@ -1,6 +0,0 @@
-#include "testclass.h"
-
-Testclass::Testclass(QObject *parent) : QObject(parent)
-{
-
-}
